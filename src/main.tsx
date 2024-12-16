a476db704de82f5f2c5ef682a5167d6c3f397f11
bajajku <bajajku@sheridancollege.ca>
2024-12-16 17:55:54 -0500

    add OrbitControls and lighting to three.js scene; update material type

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from '@react-three/fiber';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -11,16 +12,32 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// wrapping paper for geometry
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
+
+// add helper to scene
+const gridHelper = new THREE.GridHelper(200, 50);
+// add light to scene
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(pointLight, ambientLight);
 camera.position.z = 5;
 
+scene.add(gridHelper, lightHelper);
+// add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
+  requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
-  cube.rotation.y += 0.9;
+  cube.rotation.y += 0.01;
+
+  controls.update();
   renderer.render(scene, camera);
 }
-renderer.setAnimationLoop(animate);
+animate();
